eca83a7fc6df013f36661b2094abaa6793a620db
Kevin Roberts <kevin.roberts@icfolson.com>
2017-03-16 10:15:05 -0500

    fix year in footer

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -3,10 +3,11 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
 
   render () {
+    const CURRENT_YEAR = new Date().getFullYear()
     return (
       <footer className='footer container-fluid'>
         <div className='container'>
-          <p className='pull-left'>Copyright &copy; <a href='http://www.kevinroberts.us'>Vinberts</a> 2016. All rights reserved.</p>
+          <p className='pull-left'>Copyright &copy; <a href='http://www.kevinroberts.us'>Vinberts</a> {CURRENT_YEAR}. All rights reserved.</p>
           <div className='pull-right'>
             <ul className='nav nav-pills'>
               <li className='nav-item'><a href='#asdf'>Privacy</a></li>
